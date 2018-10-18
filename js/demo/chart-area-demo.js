bcb3acda8064d5725482f6071c65568f27277dd4
Jannick Hald <Jannick.hald@haldsdata.dk>
2018-10-18 10:23:20 +0200

    Update files

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [0,0,1,0,0,0,0,0,0,0,0,0,0],
+      data: [0,0,0,0,0,0,0,0,0,0,0,0,0],
     }],
   },
   options: {
